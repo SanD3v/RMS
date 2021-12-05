02ce9e3a0cb17226e663ea8ce43b25235e879f25
Bangari Devadig <santosh.bangari-devadig@capgemini.com>
2021-12-05 11:52:35 +0530

    additional changes

--- src/main/java/com/rms/RestaurantManagementSystemApplication.java
+++ src/main/java/com/rms/RestaurantManagementSystemApplication.java
@@ -2,6 +2,8 @@ package com.rms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
@@ -9,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class RestaurantManagementSystemApplication {
+public class RestaurantManagementSystemApplication extends SpringBootServletInitializer {
 	@Bean
 	public CommonsRequestLoggingFilter requestLoggingFilter() {
 		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
@@ -19,6 +21,12 @@ public class RestaurantManagementSystemApplication {
 		loggingFilter.setIncludeHeaders(false);
 		loggingFilter.setMaxPayloadLength(10000);
 		return loggingFilter;
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(RestaurantManagementSystemApplication.class);
 	}
 
 	public static void main(String[] args) {
